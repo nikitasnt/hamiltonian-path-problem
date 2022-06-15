17cb33d382088bcf9b0664935b2deec6b0c5c4cc
Nikita Pirozhkov <14nikita14m@gmail.com>
2022-06-15 18:50:48 +0400

    Edge realization

--- HamiltonianPathProblem/UndirectedGraphEdge.cs
+++ HamiltonianPathProblem/UndirectedGraphEdge.cs
@@ -4,6 +4,22 @@ public partial class UndirectedGraph
 {
     private class Edge
     {
-        
+        public Vertex Vertex1 { get; }
+        public Vertex Vertex2 { get; }
+
+        public Edge(Vertex vertex1, Vertex vertex2)
+        {
+            // Sort vertices via their numbers
+            if (vertex1.Number < vertex2.Number)
+            {
+                Vertex1 = vertex1;
+                Vertex2 = vertex2;
+            }
+            else
+            {
+                Vertex2 = vertex1;
+                Vertex1 = vertex2;
+            }
+        }
     }
 }
